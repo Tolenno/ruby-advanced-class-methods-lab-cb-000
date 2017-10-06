409b9850ba5d59c525a591ca60b349c5d39e798d
Matt Hough <matt@hough.tv>
2017-10-06 22:00:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,5 +33,8 @@ class Song
   end
 
   def self.find_or_create_by_name(song_name)
-    if @@all.include
+    if self.find_by_name(song_name)
+      self.find_by_name(song_name)
+    else
+      self.create_by_name(song_name)
   end
