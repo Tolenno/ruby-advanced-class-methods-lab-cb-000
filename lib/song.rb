cb30cad59049c23c90a297fc9b3573fd26b4621e
Matt Hough <matt@hough.tv>
2017-10-06 22:27:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
   def self.new_from_filename(file_name)
     names = file_name.split(" - ")
     self.find_or_create_by_name(names[1].chomp(".mp3"))
-    @@all[-1].artist_name = names[0]
+    #@@all[-1].artist_name = names[0]
   end
 end
 
